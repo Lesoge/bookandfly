4b64b644ffc1a88637d98c45fd6f325329c98db8
Muppet <falconlu2000@gmail.com>
2021-11-09 15:05:51 +0100

    Db Model

--- User.py
+++ User.py
@@ -14,7 +14,7 @@ class User(db.Model, UserMixin):
     username = db.Column(db.String(1024), nullable=False, unique=True)
     password = db.Column(db.String(1024), nullable=False)
     email = db.Column(db.String(1024), nullable=True, unique=False)
-
+    # relation booked flights
     # first_name = db.Column(db.String(1024), nullable=False)
     # last_name = db.Column(db.String(1024), nullable=False)
     # role_id = db.Column(UUID(as_uuid=True), db.ForeignKey('roles.id'))
@@ -33,3 +33,35 @@ class User(db.Model, UserMixin):
 
     def check_password(self, password):
         return check_password_hash(self.password, password)
+
+class Flight(db.Model):
+    __tablename__ = 'flights'
+    id = db.Column(
+        db.Integer,
+        primary_key=True,
+        autoincrement=True)
+    DepAirport = db.Column(db.String(1024), nullable=False, unique=True)
+    ArrAirport = db.Column(db.String(1024), nullable=False, unique=True)
+    # DepTime =
+    # ArrTime =
+    # Plane =
+
+class Airport(db.Model):
+    id = db.Column(
+        db.Integer,
+        primary_key=True,
+        autoincrement=True)
+
+
+class Plane(db.Model):
+    id = db.Column(
+        db.Integer,
+        primary_key=True,
+        autoincrement=True)
+
+
+class Booking(db.Model):
+    id = db.Column(
+        db.Integer,
+        primary_key=True,
+        autoincrement=True)
