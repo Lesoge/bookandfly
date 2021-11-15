3f335193b66b5f4ede70b2515330b45daa1c73c0
alexsc23 <s191825@student.dhbw-mannheim.de>
2021-11-15 15:12:56 +0100

    Added flight table

--- routes/main.py
+++ routes/main.py
@@ -1,15 +1,18 @@
 from flask import Blueprint, render_template
 from flask_login import login_required, current_user
 
+from User import Flight
+
 app_main = Blueprint('app_main', __name__)
 
 
 @app_main.route('/', methods=['GET'])
 def index():
-    return render_template('index.html')
+    flights = Flight.query
+    return render_template('index.html', flights=flights)
 
 
 @app_main.route('/profile', methods=['GET'])
 @login_required
 def profile():
-    return render_template('profile.html', name=current_user.username)
+    return render_template('profile.html', user=current_user)
