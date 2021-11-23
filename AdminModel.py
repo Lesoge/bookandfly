99b803623134cff004409cc68483783b5fd028d3
Muppet <falconlu2000@gmail.com>
2021-11-23 16:59:11 +0100

    Introduce Admin View

---
+++ AdminModel.py
@@ -0,0 +1,22 @@
+from flask_admin.contrib.sqla import ModelView
+from User import db, User, Role, Flight, Airport, Plane, Booking
+from flask_login import current_user
+from flask_admin.form import SecureForm
+
+
+# Flask and Flask-SQLAlchemy initialization here
+class AdminModelView(ModelView):
+    from_base_class = SecureForm
+
+    def is_accessible(self):
+        return (current_user.is_authenticated and
+                current_user.role.name == 'admin')
+
+
+def create_admin(admin):
+    admin.add_view(AdminModelView(User, db.session))
+    admin.add_view(AdminModelView(Role, db.session))
+    admin.add_view(AdminModelView(Flight, db.session))
+    admin.add_view(AdminModelView(Airport, db.session))
+    admin.add_view(AdminModelView(Plane, db.session))
+    admin.add_view(AdminModelView(Booking, db.session))
--- app.py
+++ app.py
@@ -6,12 +6,11 @@ from routes.auth import app_auth
 from routes.main import app_main
 from routes.mfa import app_mfa
 from User import db, User
-from flask_jwt_extended import (
-    JWTManager
-)
-
+from flask_admin import Admin
+from AdminModel import create_admin
+from OpenSSL import SSL
 login_manager = LoginManager()
-
+admin = Admin()
 
 def create_app():
     main_app = Flask(__name__)
@@ -24,18 +23,26 @@ def create_app():
     main_app.config['SQLALCHEMY_ECHO'] = echomode
     main_app.config['SECRET_KEY'] = Secret_Key
     main_app.config['JWT_SECRET_KEY'] = JWT_Key
-
+    admin.init_app(main_app)
+    create_admin(admin)
     db.init_app(main_app)
     login_manager.login_view = 'app_auth.login'
     login_manager.init_app(main_app)
+
 
     @login_manager.user_loader
     def load_user(user_id):
         return User.query.get(user_id)
 
     return main_app
-
+#
+# def create_context():
+#     context = SSL.Context(SSL.TLSv1_2_METHOD)
+#     context.use_certificate('certfile')
+#     context.use_privatekey('privkey')
+#
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
+
     create_app().run(debug=True)
