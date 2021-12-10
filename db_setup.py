505021a8da4e2d44eab759c2e055df6719a78aad
Muppet <falconlu2000@gmail.com>
2021-12-10 16:18:08 +0100

    introduce db.setup

--- db_setup.py
+++ db_setup.py
@@ -1,9 +1,12 @@
 import getpass
+import os
+import secrets
 
+import datetime
 from flask_security import hash_password
 
 from app import create_app, user_datastore
-from User import db
+from User import db, Airport, Plane, db_commit, Flight
 
 
 def create_standard_admin():
@@ -13,10 +16,38 @@ def create_standard_admin():
     user_datastore.find_or_create_role(name='admin', description='Administrator')
     user_datastore.find_or_create_role(name='end-user', description='End user')
     print('Create Standard Admin')
-    admin_username, admin_email, admin_password = get_admin_acc()
+    admin_username, admin_email, admin_password = get_admin_acc2()
     admin_password = hash_password(admin_password)
-    user_datastore.create_user(username=admin_username, email=admin_email, password=admin_password, roles= ['admin'])
+    user_datastore.create_user(username=admin_username, email=admin_email, password=admin_password, roles=['admin'])
     user_datastore.commit()
+    create_data()
+
+
+def create_data():
+    airport1 = Airport(town='Stuttgart', country='Germany', iata='STR', name='Manfred Rommel Flughafen')
+    airport2 = Airport(town='Bangkok', country='Thailand', iata='DMK', name='Suvarnabhumi International Airport ')
+    airport3 = Airport(town='Pjöngjang', country='Nord-Korea', iata='FNJ', name='Sunan')
+    airport4 = Airport(town='Peking', country='China', iata='PKX', name='Flughafen Peking-Daxing')
+    airport5 = Airport(town='Palma', country='Spanien', iata='PMI', name='Palma de Mallorca')
+
+    plane1 = Plane(planename='Boeing AH-64', seats='1')
+    plane2 = Plane(planename='Eurofighter Typhoon', seats='1')
+    plane3 = Plane(planename='R-7', seats='4')
+    plane4 = Plane(planename='F-117 B Nighthawk', seats='1')
+    plane5 = Plane(planename='NCC-1701-D', seats='430')
+
+    flight1 = Flight(depAirport=airport1, arrAirport=airport1, depTime=datetime.datetime.utcfromtimestamp(1670597890),
+                     arrTime=datetime.datetime.utcfromtimestamp(1670697890), plane=plane1, price=2000)
+    flight2 = Flight(depAirport=airport1, arrAirport=airport2, depTime=datetime.datetime.utcfromtimestamp(1672345513),
+                     arrTime=datetime.datetime.utcfromtimestamp(1672545513), plane=plane2, price=30000)
+    flight3 = Flight(depAirport=airport1, arrAirport=airport3, depTime=datetime.datetime.utcfromtimestamp(1670890213),
+                     arrTime=datetime.datetime.utcfromtimestamp(1670990213), plane=plane3, price=900000)
+    flight4 = Flight(depAirport=airport1, arrAirport=airport4, depTime=datetime.datetime.utcfromtimestamp(1670591234),
+                     arrTime=datetime.datetime.utcfromtimestamp(1670791234), plane=plane4, price=30000)
+    flight5 = Flight(depAirport=airport1, arrAirport=airport5, depTime=datetime.datetime.utcfromtimestamp(1670593713),
+                     arrTime=datetime.datetime.utcfromtimestamp(1670693713), plane=plane5, price=0)
+    db_commit(airport1, airport2, airport3, airport4, airport5, plane1, plane2, plane3, plane4, plane5, flight1,
+              flight2, flight3, flight4, flight5)
 
 
 def get_admin_acc():
@@ -25,6 +56,10 @@ def get_admin_acc():
     admin_email = input('Admin email:')
     admin_password = getpass.getpass(prompt='Admin Password:', stream=None)
     return admin_username, admin_email, admin_password
+
+
+def get_admin_acc2():
+    return 'admin', 'admin@admin.de', '1234'
 
 
 if __name__ == '__main__':
